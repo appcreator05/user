1c9abbcb2419e8e63c63bf2401590bcaaa90c4b6
appcreator05 <installapkapps@gmail.com>
2026-02-05 21:43:18 +0530

    Add files via upload

---
+++ poad.js
@@ -0,0 +1,17 @@
+// Ads Loader Script
+
+var s1 = document.createElement("script");
+s1.innerHTML = `
+atOptions = {
+  'key' : '92569caa29c97be0ba5ca5837131631c',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
+};
+`;
+document.body.appendChild(s1);
+
+var s2 = document.createElement("script");
+s2.src = "https://deridenightingalepartial.com/92569caa29c97be0ba5ca5837131631c/invoke.js";
+document.body.appendChild(s2);
