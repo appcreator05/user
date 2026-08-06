1cd9d30390f6bbffbcd6f157473cf90f8ba9e272
appcreator05 <installapkapps@gmail.com>
2026-08-07 01:02:42 +0530

    Add Firebase imports to cleanup.js

---
+++ expiry/cleanup.js
@@ -0,0 +1,2 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, get, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
